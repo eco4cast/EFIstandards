b3307633a539115fb9575509b179fd8db73c7b54
Michael Dietze <dietze@bu.edu>
2020-04-24 09:57:24 -0400

    move forecast validator helper functions to their own file

--- R/forecast_validator.R
+++ R/forecast_validator.R
@@ -1,47 +1,13 @@
-lexists <- function(list,name){
-  name %in% names(list)
-}
-
-check_exists <- function(list,name){
-  if(lexists(list,name)) usethis::ui_done(paste(name,"found"))
-  else usethis::ui_stop(paste("file missing",name))
-}
-
-check_parsable <- function(list,name,required=TRUE){
-  if(lexists(list,name)){
-    if(udunits2::ud.is.parseable(list[[name]])){
-      usethis::ui_done(paste(name,"parsable"))
-    } else {
-      usethis::ui_stop(paste(name,"not parsable"))
-    }
-  } else if(required) usethis::ui_stop(paste("file missing",name))
-}
-
-check_whole <- function(list,name,required=TRUE){
-  is.whole <- function(x){
-    x = as.numeric(x)
-    is.numeric(x) && floor(x)==x
-  }
-  if(lexists(list,name)){
-    if(is.whole(list[[name]])){
-      usethis::ui_done(paste(name,"valid"))
-    } else {
-      usethis::ui_stop(paste(name,list[[name]],"not valid, must be a whole number"))
-    }
-  } else if(required) usethis::ui_stop(paste("file missing",name))
-}
-
-
 #' EFI forecast standard EML metadata validator
 #'
 #' @param eml
 #'
-#' @return
+#' @return boolean
 #' @export
 #'
 #' @examples
 #'
-#' forecast_validator("forecast-eml.xml")
+#' forecast_validator(system.file("vignettes", "forecast-eml.xml",package="EFIstandards"))
 #'
 forecast_validator <- function(eml){
 
@@ -156,4 +122,4 @@ forecast_validator <- function(eml){
 }
 
 
-forecast_validator("forecast-eml.xml")
+#forecast_validator("forecast-eml.xml")
